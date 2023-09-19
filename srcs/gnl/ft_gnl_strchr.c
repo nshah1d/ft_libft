e9d55435773f23b1ff5ab07de5269854fc205b99
Nauman Shahid <nshahid@lab1r4s5.42abudhabi.ae>
2023-09-19 15:57:38 +0400

    Manual Update

--- srcs/gnl/ft_gnl_strchr.c
+++ srcs/gnl/ft_gnl_strchr.c
@@ -6,11 +6,39 @@
 /*   By: nshahid <nshahid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 13:24:24 by nshahid           #+#    #+#             */
-/*   Updated: 2023/09/18 13:24:25 by nshahid          ###   ########.fr       */
+/*   Updated: 2023/09/19 13:20:46 by nshahid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The `ft_gnl_strchr` function searches for the first occurrence of the
+** character 'c' in the string 'str'.
+**
+** Parameters:
+** str - The string to be searched.
+** c - The character to search for.
+**
+** Return:
+** If 'c' is found in 'str', a pointer to the first occurrence of 'c' is
+** returned.
+** If 'c' is not found, or if 'str' is NULL, the function returns NULL.
+**
+** Description:
+** The `ft_gnl_strchr` function iterates through the characters in the string
+** 'str' until it reaches the end of the string or until it finds the
+** character 'c'. If 'c' is found, a pointer to that character in the string
+** is returned. If 'c' is not found, or if 'str' is NULL, the function
+** returns NULL.
+**
+** Example:
+** const char *str = "Hello, World!";
+** char *result = ft_gnl_strchr(str, 'W');
+** Returns a pointer to 'W'
+** char *result2 = ft_gnl_strchr(str, 'X');
+** Returns NULL
+*/
 
 char	*ft_gnl_strchr(char const *str, int c)
 {
--- srcs/gnl/ft_gnl_strjoin.c
+++ srcs/gnl/ft_gnl_strjoin.c
@@ -6,11 +6,40 @@
 /*   By: nshahid <nshahid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 10:01:47 by nshahid           #+#    #+#             */
-/*   Updated: 2023/09/18 10:04:04 by nshahid          ###   ########.fr       */
+/*   Updated: 2023/09/19 14:18:07 by nshahid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** The `ft_gnl_strjoin` function concatenates two strings: 'buffer' and
+** 'content', into a new string.
+**
+** Parameters:
+**  buffer - The first string to be concatenated.
+**  content - The second string to be concatenated.
+**
+** Return:
+**  If memory allocation fails or if both 'buffer' and 'content' are NULL,
+**  the function returns NULL.
+**  Otherwise, it returns a newly allocated string containing the
+**  concatenated result.
+**
+** Description:
+**  The `ft_gnl_strjoin` function calculates the lengths of 'buffer' and
+**  'content' and allocates memory for a new string that can accommodate both
+**  strings. It then copies the characters from 'buffer' and 'content' into
+**  the new string and ensures it is null-terminated. Finally, it frees the
+**  memory occupied by the original 'buffer' and returns the new concatenated
+**  string.
+**
+** Example:
+**  char *buffer = "Hello, ";
+**  const char *content = "World!";
+**  char *result = ft_gnl_strjoin(buffer, content);
+**  Returns "Hello, World!"
+*/
 
 char	*ft_gnl_strjoin(char *buffer, const char *content)
 {
